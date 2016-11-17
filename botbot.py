f0640d2d7a4f64b1b83da9bb9376867724a229e1
Mark <markh@columbia.edu>
2016-11-17 06:29:53 -0500

    Update botbot.py

--- botbot.py
+++ botbot.py
@@ -1,11 +1,23 @@
 from os import environ
-from flask import Flask
+from time import sleep
+from slackclient import SlackClient
 
-app = Flask("botbot")
+slack_token = environ.get("SLACK_TOKEN")
+bot_id = environ.get("BOT_ID")
 
-@app.route("/")
-def wakeup():
-    return "Hello. I'm Mark's bot!"
+at_bot = "<@" + bot_id + ">"
+sc = SlackClient(slack_token)
 
-port = int(environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+delay = 1 # 1 second delay between reading from firehose
+if sc.rtm_connect():
+    print("StarterBot connected and running!")
+    while True:
+        output_list = sc.rtm_read()
+        if output_list and len(output_list) > 0:
+            for output in output_list:
+                if output and 'text' in output and at_bot in output['text']:
+                    # return text after the @ mention, whitespace removed
+                    print output['text']
+        sleep(delay)
+else:
+    print "Connection failed. Invalid Slack token or bot ID?"
